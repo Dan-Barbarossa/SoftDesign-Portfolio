f0ee9d4657b4e544f96ec81ee445071aa1c08bd8
Dan-Barbarossa <70698389+Dan-Barbarossa@users.noreply.github.com>
2026-01-06 15:25:36 +0800

    pt1: pw-gen v1 completed

--- password-generator/src/PasswordGenerator.jsx
+++ password-generator/src/PasswordGenerator.jsx
@@ -1,50 +1,102 @@
 import React from 'react'
+import { useState } from 'react'
 import ReactSwitch from 'react-switch'
 
 const PasswordGenerator = () => {
+
+  const [password, setPassword] = useState("Password");
+  const [passwordLength, setPasswordLength] = useState(true);
+  const [includeUpperCase, setIncludeUpperCase] = useState(true);
+  const [includeNumbers, setIncludeNumbers] = useState(true);
+  const [includeSpecialChars, setIncludeSpecialChars] = useState(true);
+
+  const generatePassword = () => {
+    const lowerCaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    const upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numbers = '1234567890';
+    const specialChars = '!@#$%^&*()_+';
+
+    let validChars = lowerCaseChars;
+
+    if (includeUpperCase) {
+      validChars += upperCaseChars;
+    }
+
+    if (includeNumbers) {
+      validChars += numbers;
+    }
+
+    if (includeSpecialChars) {
+      validChars += specialChars;
+    }
+
+    let generatePassword = "";
+    for (let i = 0; i < passwordLength; i++) {
+      const randomIndex = Math.floor(Math.random() * validChars.length);
+      generatePassword += validChars.charAt(randomIndex);
+    }
+
+    console.log(generatePassword);
+    setPassword(generatePassword);
+  }
+
+  //generatePassword();
+
   return (
     <div className='pt-8 text-white font-bold'>
-      <div className= 'flex flex-col items-center'>
-        <div className='bg-purple-800 w-[350px] shadow-2xl hover:shadow-blue-900 rounded'>
+      <div className='flex flex-col items-center'>
+        <div className="bg-purple-800 w-[350px] shadow-2xl
+         hover:shadow-blue-900 rounded-md p-3">
 
-        
-      <div>
-      <label>Password Length: </label>
-      <input 
-      className = 'w-12 bg-purple-700 pl-2'
-      type='number'
-      />
-      </div>
-      <div>
-        <label>Include Uppercase: </label>
-        <ReactSwitch 
-        height = {20}
-        width = {40}
-        handleDiameter={18}
-        />
-      </div>
 
-      <div>
-        <label>Include Numbers: </label>
-        <ReactSwitch 
-        height = {20}
-        width = {40}
-        handleDiameter={18}
-        />
-      </div>
+          <div className="mb-4 flex items-center justify-between">
+            <label>Password Length: </label>
+            <input
+            value = {passwordLength}
+            onChange={(e)=>setPasswordLength(e.target.value)}
+              className='w-12 bg-purple-700 pl-2'
+              type='number'
+            />
+          </div>
 
-        <div>
-        <label>Include Special Characters: </label>
-        <ReactSwitch 
-        height = {20}
-        width = {40}
-        handleDiameter={18}
-        />
-      </div>
 
-      <button> Generate Password </button>
-    </div>
-    </div>
+          <div className="mb-4 flex item-center justify-between">
+            <label>Include Uppercase: </label>
+            <ReactSwitch
+              checked={includeUpperCase}
+              onChange={() => setIncludeUpperCase(!includeUpperCase)}
+
+              height={20} width={40} handleDiameter={18}
+            />
+          </div>
+
+          <div className="mb-4 flex item-center justify-between">
+            <label>Include Numbers: </label>
+            <ReactSwitch
+              checked={includeNumbers}
+              onChange={() => setIncludeNumbers(!includeNumbers)}
+
+              height={20} width={40} handleDiameter={18}
+            />
+          </div>
+
+          <div className="mb-4 flex item-center justify-between">
+            <label>Include Special Characters: </label>
+            <ReactSwitch
+              checked={includeSpecialChars}
+              onChange={() => setIncludeSpecialChars(!includeSpecialChars)}
+              height={20} width={40} handleDiameter={18}
+            />
+          </div>
+
+          <button 
+          onClick={generatePassword}
+          className="bg-gradient-to-r from-green-400 to-blue-500
+       hover:from-pink-500 hover:to-yellow-500 p-2 text-white font-bold 
+       rounded-md"> Generate Password </button>
+        {password && <p className="font-normal text-white mt-2"> Generate Password: <span className='font-bold'>{password}</span></p>}
+      </div>
+      </div>
     </div>
   )
 }
